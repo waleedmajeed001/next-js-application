f632d40c9ba987829ac654676292b887e5155cb6
waleedmajeed001 <147373175+waleedmajeed001@users.noreply.github.com>
2024-04-17 11:52:21 +0500

    next-js-application

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Dashboard() {
   return(
   <main className="p-5">
-  <p className="font-bold"> This Dashboard Page🤗 </p>
+  <p className="font-bold"> This is Dashboard Page🤗 </p>
   <div className="flex gap-5 flex-col mt-5">
     <Link href={"/dashboard/analytics"}>Analytics</Link>
     <Link href={"/dashboard/settings"}>Settings</Link>
